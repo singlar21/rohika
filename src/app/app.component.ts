18e35c6d8113a8af35b986aff0c075f2992ed870
Rohit Singla <aarav@Rohits-MacBook-Air.local>
2025-01-09 18:39:52 +0530

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CreateUsersComponent } from './users/create-users/create-users.component';
+import { NotificationComponent } from './core/notification/notification.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CreateUsersComponent,NotificationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.less'
 })
